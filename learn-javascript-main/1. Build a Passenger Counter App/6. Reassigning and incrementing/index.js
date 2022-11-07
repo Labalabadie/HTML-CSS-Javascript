7b7b50faf8700df7e96d2d22e3589405405ae6fc
Agustin Labadie <labalabadie@gmail.com>
2022-11-07 20:08:59 -0300

    First commit but already some work done

---
+++ learn-javascript-main/1. Build a Passenger Counter App/6. Reassigning and incrementing/index.js
@@ -0,0 +1,24 @@
+// document.getElementById("count").innerText = 5
+
+let count = 5
+
+console.log(count)
+
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+let bonusPoints = 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints * 2
+
+console.log(bonusPoints)
+
+// Decrease it down to 25, and then finally increase it to 75
+bonusPoints = bonusPoints / 4
+
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + bonusPoints * 2
+
+console.log(bonusPoints)
+
+
