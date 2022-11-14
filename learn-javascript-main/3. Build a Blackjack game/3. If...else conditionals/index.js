0c38849847233c8fcc0b325a3a3d63c23ea4223d
Agustin Labadie <labalabadie@gmail.com>
2022-11-14 10:45:01 -0300

    Making simple black jack app

--- learn-javascript-main/3. Build a Blackjack game/3. If...else conditionals/index.js
+++ learn-javascript-main/3. Build a Blackjack game/3. If...else conditionals/index.js
@@ -2,3 +2,13 @@ let firstCard = 6
 let secondCard = 9
 let sum = firstCard + secondCard
 
+if (sum < 21) {
+    console.log("Do you want another card? 🤔")
+}
+if (sum > 21) {
+    console.log("Oops! You've lost 😅")
+}
+if (sum === 21) {
+    console.log("Conga Rats! You've got BlackJack 🪘🐭")
+}
+
