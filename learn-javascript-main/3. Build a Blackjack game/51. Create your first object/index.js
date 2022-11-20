750f79557f6bc4dc52ccc679ec85011ec9e01841
Agustin Labadie <labalabadie@gmail.com>
2022-11-20 20:45:08 -0300

    51

--- learn-javascript-main/3. Build a Blackjack game/51. Create your first object/index.js
+++ learn-javascript-main/3. Build a Blackjack game/51. Create your first object/index.js
@@ -1,5 +1,17 @@
 // Create an object that represents an airbnb castle listing.
 // It should contain at least one boolean, one string, one number, and one array
 // Log out at least two of the keys using the dot notation
+let castle = {
+    name: "Ratimbun",
+    rooms: 19,
+    country: "Italy",
+    age: 451,
+    isAvailable: true,
+    catsLivingInThere: ["Misifus", "Michuli", "Señor bigotes"]
+}
+for (let i = 0 ; i < castle.catsLivingInThere.length ; i++) {
+    console.log(castle.catsLivingInThere[i])
+}
 
+console.log(castle.name)
 
